d4f21a5c315776c396993df8e0c4d41661d2c02c
jdb-codes <jdbrown.tech@gmail.com>
2025-05-18 23:06:43 -0700

    Added rotation of player object

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("#000000")
         player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
